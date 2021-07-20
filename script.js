7b59cbbf63ea036af67c3aaca384f6ebd50bca84
Ishu Bansal <Bansalishu23@gmail.com>
2021-07-21 01:01:16 +0530

    proj one complete

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // challenge 1:
+function ageInDays()
+{
+ var birthyear = prompt("Enter your birth year");
+ var age = 2021 - birthyear;
+ var days = ((age*365) + (6*age)/24);
+var h1 = document.createElement('h1');
 
-var age = prompt("Enter your age");
+var textAns = document.createTextNode('you are '+ days + ' days old');
+h1.setAttribute('id','ageInDays');
+h1.appendChild(textAns);
+document.getElementById('flex-box-result').appendChild(h1);
 
-var days = ((age*365) + (6*age)/24); 
+}
+ 
 
-document.getElementById("days").innerHTML = days;
+function reset()
+{
+ document.getElementById('ageInDays').remove();
+}
+// document.getElementById("days").innerHTML = days;
