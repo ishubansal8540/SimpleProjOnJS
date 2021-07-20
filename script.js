7f42cafc498de750d1b094a6a5936376ca02abcd
Ishu Bansal <Bansalishu23@gmail.com>
2021-07-21 01:36:23 +0530

    proj 2 struct complete

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ document.getElementById('flex-box-result').appendChild(h1);
 
 }
  
-
 function reset()
 {
  document.getElementById('ageInDays').remove();
 }
+
+
+
 // document.getElementById("days").innerHTML = days;
